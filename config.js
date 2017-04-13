e506f49c46a821177de366cc255c64b24c73866e
basedghoul <schelale17@gmail.com>
2017-04-12 21:30:02 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+{
+  "bot_id"  : "3706613804657289cb5f13f8be",
+  "text"    : "TGIF",
+  "attachments" : [
+    {
+      "type"  : "image",
+      "url"   : "https://pbs.twimg.com/media/CwcEf3CWEAAtHGW.jpg"
+    }
+  ]
+}
